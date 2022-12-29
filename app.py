ac17d274c9fd54b5fc555d0b9d63f24fafe68ad4
omar lamine <76263000+omarlamin01@users.noreply.github.com>
2022-12-29 22:30:59 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,12 @@
+import string
+
+
+def cesar_encrypt(text, key):
+    result = ""
+    for i in range(len(text)):
+        char = text[i]
+        if char.isupper():
+            result += chr((ord(char) + key - 65) % 26 + 65)
+        else:
+            result += chr((ord(char) + key - 97) % 26 + 97)
+    return result
